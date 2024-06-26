25acb8751915f5221f417e6792dd78c79232001a
unknown <stayon2@yandex.ru>
2024-06-26 15:12:25 +0300

    +setUsers - first render

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,8 @@ import Class from './Users.module.css';
 
 let Users = (props) => {
 
-  let users = props.users.map(u => 
+
+  let users2 = props.users.map(u => 
     <div key={u.id} className={Class.users}>
       <div>{u.fullName}</div>
       <div>{u.status}</div>
@@ -17,8 +18,19 @@ let Users = (props) => {
     </div>
   )
 
+    if (users2.length === 0) {
+    props.setUsers(
+      [{id: 1, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
+      followed: true, fullName: 'Jake', status: 'This is a sheriff', location: {country: 'France', city: 'Paris'}},
+    {id: 2, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
+      followed: false, fullName: 'Jimmy', status: 'Super human', location: {country: 'Italy', city: 'Rome'}},
+    {id: 3, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
+      followed: true, fullName: 'Bobby', status: 'Just person', location: {country: 'Russia', city: 'Moscow'}},
+    ])
+  }
+
   return (
-    <div>{users}</div>
+    <div>{users2}</div>
   )
 }
 
--- src/components/redux/usersReducer.js
+++ src/components/redux/usersReducer.js
@@ -3,23 +3,14 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-  users: [
-    {id: 1, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
-      followed: true, fullName: 'Jake', status: 'This is a sheriff', location: {country: 'France', city: 'Paris'}},
-    {id: 2, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
-      followed: false, fullName: 'Jimmy', status: 'Super human', location: {country: 'Italy', city: 'Rome'}},
-    {id: 3, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
-      followed: true, fullName: 'Bobby', status: 'Just person', location: {country: 'Russia', city: 'Moscow'}},
-    ]
+  users: []
 };
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case FOLLOW: 
       return {
-        ...state,
-        //users: [...state.users]  =  users: state.users.map(u => u);
-
+        ...state, //users: [...state.users]  =  users: state.users.map(u => u);
         users: state.users.map(u => {
           if (u.id === action.userId) {
             return {...u, followed: true}
