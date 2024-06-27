827be2817bdf476cded0fd88e819e1f40117e318
unknown <stayon2@yandex.ru>
2024-06-27 15:04:10 +0300

    +axios, get users from api

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,38 +1,38 @@
 import Class from './Users.module.css';
+import axios from 'axios';
+import userPhoto from '../../images/user.png';
 
 let Users = (props) => {
 
-
-  let users2 = props.users.map(u => 
-    <div key={u.id} className={Class.users}>
-      <div>{u.fullName}</div>
-      <div>{u.status}</div>
-      <div>{u.location.city}</div>
-      <div>{u.location.country}</div>
-      <div><img src={u.fotoUrl} className={Class.userPhoto} /></div>  
-      <div> 
-        { u.followed 
-          ? <button onClick={()=> {props.unfollow(u.id)}}>Unfollow</button>
-          : <button onClick={()=> {props.follow(u.id)}}>Follow</button> }
-      </div>
-    </div>
-  )
-
-    if (users2.length === 0) {
-    props.setUsers(
-      [{id: 1, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
-      followed: true, fullName: 'Jake', status: 'This is a sheriff', location: {country: 'France', city: 'Paris'}},
-    {id: 2, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
-      followed: false, fullName: 'Jimmy', status: 'Super human', location: {country: 'Italy', city: 'Rome'}},
-    {id: 3, fotoUrl: 'https://t4.ftcdn.net/jpg/05/66/05/55/360_F_566055532_TVl1zRxXpTJYVHAq8RwrI2r2LQvu7oN3.jpg',
-      followed: true, fullName: 'Bobby', status: 'Just person', location: {country: 'Russia', city: 'Moscow'}},
-    ])
-  }
-
-  return (
-    <div>{users2}</div>
-  )
-}
+  if (props.users.length === 0) {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        props.setUsers(response.data.items)
+      })         
+  }  
+  
+  return  <div className={Class.users}>
+    { 
+      props.users.map(u => 
+        <div key={u.id}>
+          <div>
+            <img src={u.photos.small !=null ? u.photos.small : userPhoto} className={Class.userPhoto} />
+          </div>
+          <div>{u.name}</div>
+          <div>{u.status}</div>
+          <div>{'u.location.city'}</div>
+          <div>{'u.location.country'}</div>
+          <div>
+            { u.followed 
+              ? <button onClick={()=> {props.unfollow(u.id)}}>Unfollow</button>
+              : <button onClick={()=> {props.follow(u.id)}}>Follow</button>  
+            }
+          </div>  
+        </div> 
+      )
+    }   
+  </div>
+} 
 
 
 export default Users;
